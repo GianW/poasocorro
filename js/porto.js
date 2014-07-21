c2ad3087adb4a2dc3698e43fd682bd9a9601ff9b
Gian <gian.cb@hotmail.com>
2014-07-21 09:26:31 -0300

    xa

--- js/porto.js
+++ js/porto.js
@@ -2,25 +2,145 @@ var map;
 var resposta_json;
 var idInfoBoxAberto;
 var infoBox = [];
-var markers = []; 
+var markers = [];
+var latlng; 
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay;
+var geocoder;
+
+function localizaUsuario(){
+	latUsuario = document.getElementById('latUsuario');
+	longUsuario = document.getElementById('longUsuario');
+
+	geocoder = new google.maps.Geocoder();
+
+	/*Busca posição do usuário, se não usa default */ 
+	 if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {    
+	    	latlng = new google.maps.LatLng( position.coords.latitude,  position.coords.longitude);     	       
+			
+
+			latUsuario.value = position.coords.latitude;
+			longUsuario.value = position.coords.longitude;
+
+			var marker = new google.maps.Marker({
+					position: new google.maps.LatLng( position.coords.latitude,  position.coords.longitude),
+					title: "Usuário",
+					map: map,
+					draggable: true,
+					// icon: 'css/imagens/user.png',	
+					icon: 'css/imagens/userMarker2.png',				
+					animation: google.maps.Animation.DROP
+				});
+
+			google.maps.event.addListener(marker, 'drag', function () {
+		        geocoder.geocode({'latLng': marker.getPosition() }, function (results, status) {		        	
+		            if (status == google.maps.GeocoderStatus.OK) {
+		                    if (results[0]) { 
+		                    	$('#latUsuario').val(marker.getPosition().lat());			                    
+			                    $('#longUsuario').val(marker.getPosition().lng());
+		                }
+		            }
+		        });
+		    });
+			
+	    });
+	}else{
+		 latlng = new google.maps.LatLng( -30.0559805, -51.1707677);
+	}
+
+	
+}
+
+function carregaMapa(){
+
+
+google.maps.visualRefresh = true;
+
+var options = {
+	    zoom: 12,
+	    center: latlng,
+	    // scrollwheel: false,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    panControl:true,
+	    panControlOptions: {
+	        position: google.maps.ControlPosition.RIGHT_BOTTOM
+	    },
+	    mapTypeControl: true,
+	    mapTypeControlOptions: {
+	        position: google.maps.ControlPosition.TOP_RIGHT
+	    },
+	    streetViewControlOptions: {
+	        position: google.maps.ControlPosition.LEFT_BOTTOM
+	    },
+	    zoomControl: true,
+	    zoomControlOptions: {
+	        style: google.maps.ZoomControlStyle.DEFAULT, // SMALL, LARGE ou DEFAULT -> Definição da barra de zoom
+	        position: google.maps.ControlPosition.LEFT_CENTER
+	    }
+
+	}
+
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+}
+
+
 
 function initialize() {	
 
+google.maps.visualRefresh = true;
 /*Cria mapa*/
-var latlng = new google.maps.LatLng( -30.0559805, -51.1707677);
+
+
+// /*Busca posição do usuário, se não usa default */ 
+//  if (navigator.geolocation) {
+// 	navigator.geolocation.getCurrentPosition(function (position) {    
+//     	latlng = new google.maps.LatLng( position.coords.latitude,  position.coords.longitude);     	       
+// 		alert(latlng);
+// 		var marker = new google.maps.Marker({
+// 				position: new google.maps.LatLng( position.coords.latitude,  position.coords.longitude),
+// 				title: "usuario",
+// 				map: map,
+// 				//icon: 'img/marcador.png'
+// 				animation: google.maps.Animation.DROP
+// 			});
+//     });
+// }else{
+// 	 latlng = new google.maps.LatLng( -30.0559805, -51.1707677);
+// }
+
+
+
+// latlng = new google.maps.LatLng( -30.0559805, -51.1707677);
 
 var options = {
     zoom: 12,
     center: latlng,
+    // scrollwheel: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    panControl:false,
-    overViewMapControl:false
+    panControl:true,
+    panControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+    },
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    streetViewControlOptions: {
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.DEFAULT, // SMALL, LARGE ou DEFAULT -> Definição da barra de zoom
+        position: google.maps.ControlPosition.LEFT_CENTER
+    }
+    
 };
 
 map = new google.maps.Map(document.getElementById("mapa"), options);
 
 
-carregarPontos();
+carregarPontos('app/services/buscaDados.php?service=');
 
 // var styles = [{
 // 	stylers:[
@@ -39,6 +159,21 @@ carregarPontos();
 // map.mapTypes.set('map_style', styledMap);
 // map.setMapTypeId('map_style');
 
+var avaibleTags = [
+	"Hospital",
+	"Pronto Atendimento",
+	"Centro de saúde",
+	"Unidade Básica de Saúde",
+	"Estratégia de saúde da família",
+	"Atenção especializada Municipal",
+	"Atenção especializada Conveniada"
+
+];
+
+ $("#valorPesquisa").autocomplete({
+ 	source: avaibleTags
+ });
+
 }             
 
 function abrirInfoBox(id, marker) {
@@ -50,25 +185,38 @@ infoBox[id].open(map, marker);
 idInfoBoxAberto = id;
 }
 
-function carregarPontos() { 
+function carregarPontos(caminho) { 
 
-
-$.getJSON('app/buscaDados.php', function(pontos){
+$.getJSON(caminho, function(pontos){
 		
 		var latlngbounds = new google.maps.LatLngBounds();
 		
 		$.each(pontos, function(index, ponto) {
-									
+			
+			/*Trata caracteres especiais retornados pelo JSON*/
+			var tratatxt = document.getElementById('tratatexto');
+			tratatxt.innerHTML = ponto.nome;
+			ponto.nome = tratatxt.innerHTML;
+			
 			var marker = new google.maps.Marker({
 				position: new google.maps.LatLng(ponto.lat, ponto.long),
 				title: ponto.nome,
 				map: map,
-				//icon: 'img/marcador.png'
+				// icon: 'css/imagens/map-green-marker.png',
 				animation: google.maps.Animation.DROP
 			});
 			
 			var myOptions = {
-				content: "<p><h3>" + ponto.nome + "</h3>Nota:3</p><p>" + ponto.endereco + "</p><p>Especialidades</p>",
+				content: "<p><h3>" + ponto.nome + "</h3><img src='css/imagens/est_cheia.png' alt='1' height='15' width='15'/>"
+				+ "<img src='css/imagens/est_cheia.png' alt='1' height='15' width='15'/>"				
+				+ "<img src='css/imagens/est_cheia.png' alt='1' height='15' width='15'/>Nota:3"
+				// + "<input type='button' value='Rota' onclick='ponto(" + ponto.lat + "," + ponto.long + ")' /></p>"
+				+ "<input type='button' value='Carro' onclick='ponto(" + ponto.lat + "," + ponto.long + ",1)' />"
+				+ "<input type='button' value='Pé' onclick='ponto(" + ponto.lat + "," + ponto.long + ",2)' />"
+				// + "<input type='button' value='Bike' onclick='ponto(" + ponto.lat + "," + ponto.long + ",3)' />"
+				+ "<input type='button' value='Bus' onclick='ponto(" + ponto.lat + "," + ponto.long + ",4)' /></p>"
+				+ "<p>" + ponto.endereco + "</p><p>Especialidades:</p>"
+				+ "<p><a target='_blank' href=" + ponto.site + ">Site</a></p>",
 				pixelOffset: new google.maps.Size(-150, 0)
         	};
 
@@ -83,11 +231,14 @@ $.getJSON('app/buscaDados.php', function(pontos){
 			markers.push(marker);
 			
 			latlngbounds.extend(marker.position);
-		});
-		
-		 var markerCluster = new MarkerClusterer(map, markers);
+		});		
+
+
+		 // var markerCluster = new MarkerClusterer(map, markers);
 		
 		 map.fitBounds(latlngbounds);
+
+		 localizaUsuario();
 		
 	});       	        
 
@@ -133,3 +284,122 @@ function mostraResposta(requisicaoAjax) {
 	}  
 } 
 
+function pesquisa(){	
+	divTrajeto = document.getElementById("trajeto-texto");
+	divMapa = document.getElementById("mapa");
+
+	divMapa.style.width = "100%";	
+	divTrajeto.style.width = "0%";
+	divTrajeto.innerHTML = "";
+	carregaMapa();
+
+	var stringProcura = document.getElementById("valorPesquisa").value;
+	// alert(stringProcura.value.replace(/[\*]/g )); VALIDAR PESQUISA PARA SEGURANÇA
+	//pega valor do campo e converte para letras minúsculas
+      stringProcura = stringProcura.toLowerCase();
+      //faz as substituições dos acentos
+      stringProcura = stringProcura.replace(/[á|ã|â|à]/gi, "a");
+      stringProcura = stringProcura.replace(/[é|ê|è]/gi, "e");
+      stringProcura = stringProcura.replace(/[í|ì|î]/gi, "i");
+      stringProcura = stringProcura.replace(/[õ|ò|ó|ô]/gi, "o");
+      stringProcura = stringProcura.replace(/[ú|ù|û]/gi, "u");
+      stringProcura = stringProcura.replace(/[ç]/gi, "c");
+      stringProcura = stringProcura.replace(/[ñ]/gi, "n");
+      stringProcura = stringProcura.replace(/[á|ã|â]/gi, "a");
+
+     
+	map.clearOverlays();
+
+	carregarPontos("app/services/buscaDados.php?service=" + stringProcura + "");
+
+}
+
+google.maps.Map.prototype.clearOverlays = function() {
+  for (var i = 0; i < markers.length; i++ ) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;  
+}
+
+function ponto(lat, long, transp){
+
+	directionsDisplay = new google.maps.DirectionsRenderer();
+
+	latUsuario = document.getElementById('latUsuario').value;
+	longUsuario = document.getElementById('longUsuario').value;
+
+	var options = {
+		    zoom: 10,
+		    center: latlng,
+		    // scrollwheel: false,
+		    mapTypeId: google.maps.MapTypeId.ROADMAP,
+		    panControl:true,
+		    panControlOptions: {
+		        position: google.maps.ControlPosition.RIGHT_BOTTOM
+		    },
+		    mapTypeControl: true,
+		    mapTypeControlOptions: {
+		        position: google.maps.ControlPosition.TOP_RIGHT
+		    },
+		    streetViewControlOptions: {
+		        position: google.maps.ControlPosition.LEFT_BOTTOM
+		    },
+		    zoomControl: true,
+		    zoomControlOptions: {
+		        style: google.maps.ZoomControlStyle.DEFAULT, // SMALL, LARGE ou DEFAULT -> Definição da barra de zoom
+		        position: google.maps.ControlPosition.LEFT_CENTER
+		    }
+		    
+	};
+
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+
+	directionsDisplay.setMap(map);
+	divTrajeto = document.getElementById("trajeto-texto");
+	divMapa = document.getElementById("mapa");
+
+	divMapa.style.width = "60%";	
+	divTrajeto.style.width = "40%";
+
+	directionsDisplay.setPanel(divTrajeto); // Aqui faço a definição
+
+	if (transp == 2) {
+			var request = { // Novo objeto google.maps.DirectionsRequest, contendo:
+			      origin: new google.maps.LatLng(latUsuario, longUsuario),   	/*enderecoPartida, // origem*/
+			      destination: new google.maps.LatLng(lat, long),				/*enderecoChegada, // destino*/
+			      travelMode: google.maps.TravelMode.WALKING 					// meio de transporte
+    		};
+	}
+	/*Se escolhei bike*/
+	else if (transp == 3) {
+			var request = { // Novo objeto google.maps.DirectionsRequest, contendo:
+			      origin: new google.maps.LatLng(latUsuario, longUsuario),   	/*enderecoPartida, // origem*/
+			      destination: new google.maps.LatLng(lat, long),				/*enderecoChegada, // destino*/
+			      travelMode: google.maps.TravelMode.BICYCLING 					// meio de transporte
+    		};
+	}
+	/*Se escolheu bus */
+	else if (transp == 4) {
+			var request = { // Novo objeto google.maps.DirectionsRequest, contendo:
+			      origin: new google.maps.LatLng(latUsuario, longUsuario),   	/*enderecoPartida, // origem*/
+			      destination: new google.maps.LatLng(lat, long),				/*enderecoChegada, // destino*/
+			      travelMode: google.maps.TravelMode.TRANSIT 					// meio de transporte
+    		};
+	}
+	/*se não vai de carro*/
+	else{
+		var request = { // Novo objeto google.maps.DirectionsRequest, contendo:
+	      origin: new google.maps.LatLng(latUsuario, longUsuario),   	/*enderecoPartida, // origem*/
+	      destination: new google.maps.LatLng(lat, long),				/*enderecoChegada, // destino*/
+	      travelMode: google.maps.TravelMode.DRIVING 					// meio de transporte, nesse caso, de carro
+	    };	
+	};
+	
+
+   directionsService.route(request, function(result, status) {
+	      if (status == google.maps.DirectionsStatus.OK) { // Se deu tudo certo
+	         directionsDisplay.setDirections(result); // Renderizamos no mapa o resultado
+	      }
+   });
+	
+}
